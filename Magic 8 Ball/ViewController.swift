47b9446fc83ec1b6cb8ebc0522437b89fbb078aa
Juri Plugatariov <juri.plugatariov@heathmont.net>
2018-06-15 15:54:51 +0300

    Initial Commit

---
+++ Magic 8 Ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by Juri Plugatariov on 15/06/2018.
+//  Copyright © 2018 Juri Plugatariov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
